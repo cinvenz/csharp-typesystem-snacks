406a57057556c74d58e83f864501c117c7c7b7fe
cinvenz <vincenzobortune@gmail.com>
2023-03-16 14:02:53 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿snack1();
+snack2();
+snack10();
+
+void snack1()
+{
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int sum = num1 - num2;
+
+    if (num1 == num2)
+    {
+        Console.WriteLine("Numbers are equal");
+    }
+    else
+    {
+        Console.WriteLine(sum);
+    }
+}
+
+void snack2()
+{
+    string str1 = Console.ReadLine() ?? "";
+    string str2 = Console.ReadLine() ?? "";
+
+    if (str1.Length > str2.Length)
+    {
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+    }
+    else
+    {
+        Console.WriteLine(str2);
+        Console.WriteLine(str1);
+    }
+}
+
+void snack10()
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    var random = new Random();
+
+    for (int i = 0; i < N; i++)
+    {
+        var ints = new int[10];
+
+        for (int j = 0; j < 10; j++)
+        {
+            ints[j] = random.Next(1, 100);
+        }
+
+        foreach (var num in ints)
+        {
+            Console.Write($"{num}; ");
+        }
+
+        Console.WriteLine();
+    }
+}
